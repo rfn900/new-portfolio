bdc8322bfd839437e1ef4eacc0b7f77a13fc4259
rfn900 <rfn900@gmail.com>
2021-10-22 23:58:13 +0200

    Create next app w/ tailwindcss and start design theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,35 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Open sans", "sans-serif", ...defaultTheme.fontFamily.sans],
+      mono: [
+        "Fira Code",
+        "Ubuntu",
+        "monospace",
+        ...defaultTheme.fontFamily.mono,
+      ],
+    },
+    extend: {
+      colors: {
+        warmGray: colors.warmGray,
+        themeGray: {
+          light: "#fefefe",
+          medium: "#636469",
+          base: "#2D2E32",
+          dark: "#282A2D",
+          darker: "#24262A",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
