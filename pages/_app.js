a86f9481b6bc9451c9726a81eec7c588b41f98e8
rfn900 <rfn900@gmail.com>
2021-10-25 14:10:42 +0200

    Add Dark Mode Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
-
+import { DarkModeContext } from "../context/darkMode";
+import { useState } from "react";
 function MyApp({ Component, pageProps }) {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </DarkModeContext.Provider>
   );
 }
 
