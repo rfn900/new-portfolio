e9d86cfb3f1d30ed7173439476f98fa644b1ff1b
rfn900 <rfn900@gmail.com>
2021-10-29 23:17:05 +0200

    Fix About Me Component

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,21 +1,26 @@
+import { HiOutlineCode } from "react-icons/hi";
+import { HiOutlineViewGrid } from "react-icons/hi";
+import { MdSyncProblem } from "react-icons/md";
+import { MdPictureAsPdf } from "react-icons/md";
+import AppButton from "./AppButton";
 const AboutMe = () => {
   return (
-    <div className="section mx-auto mt-12">
-      <h2 className="text-4xl font-bold w-full text-center font-mono">
+    <div className="flex-center mt-24 xs:mt-0 flex-col section">
+      <h2 className="w-full font-mono text-4xl font-bold text-center">
         Who am I?
       </h2>
-      <div className="flex flex-col w-full lg:flex-row items-center lg:items-start mt-16 md:mt-24 h-[900px] lg:h-[500px]">
-        <div className="flex-1 flex h-full lg:w-1/2 items-center flex-col ">
+      <div className="flex flex-col gap-4 lg:gap-0 w-full lg:flex-row  items-center lg:items-start mt-16 md:mt-24 h-[900px] lg:h-[500px]">
+        <div className="flex flex-col w-full items-center h-full lg:mr-12 lg:w-1/2 ">
           <div className="flex flex-col overflow-hidden max-w-[360px] md:max-w-[480px] gap-8 bg-transparent relative dark:bg-themeGray-dark p-4 rounded-xl w-full lg:w-[500px] h-full">
-            <div className="absolute inset-0 bg-themeGray-medium dark:hidden filter blur-xl opacity-50"></div>
-            <div className="w-full py-4 pr-2 lg:px-2 flex gap-0 lg:gap-2 items-center bg-themeGray-light z-10 dark:bg-themeGray-darker rounded-xl h-1/3 transition hover:shadow-xl shadow-md">
+            <div className="absolute inset-0 opacity-50 bg-themeGray-medium dark:hidden filter blur-xl"></div>
+            <div className="z-10 flex items-center w-full py-4 pr-2 shadow-md lg:px-2 gap-0 lg:gap-2 bg-themeGray-light dark:bg-themeGray-darker rounded-xl h-1/3 transition hover:shadow-xl">
               <div className="px-4">
-                <div className="h-12 w-12 lg:h-16 lg:w-16 flex-center rounded-full bg-yellow-400">
-                  <HiOutlineCode className="text-themeGray-dark h-8 w-8 lg:h-10 lg:w-10" />
+                <div className="w-12 h-12 bg-yellow-400 rounded-full lg:h-16 lg:w-16 flex-center">
+                  <HiOutlineCode className="w-8 h-8 text-themeGray-dark lg:h-10 lg:w-10" />
                 </div>
               </div>
               <div className="w-3/4 lg:w-5/6">
-                <h2 className="text-sm lg:text-xl font-mono font-bold">
+                <h2 className="font-mono text-sm font-bold lg:text-xl">
                   Web Developer
                 </h2>
                 <p className="mt-2 text-xs lg:text-base">
@@ -23,14 +28,14 @@ const AboutMe = () => {
                 </p>
               </div>
             </div>
-            <div className="w-full py-4 pr-2 lg:px-2 flex gap-0 lg:gap-2 items-center bg-themeGray-light z-10 dark:bg-themeGray-darker rounded-xl h-1/3 transition hover:shadow-xl shadow-md">
+            <div className="z-10 flex items-center w-full py-4 pr-2 shadow-md lg:px-2 gap-0 lg:gap-2 bg-themeGray-light dark:bg-themeGray-darker rounded-xl h-1/3 transition hover:shadow-xl">
               <div className="px-4">
-                <div className="h-12 w-12 lg:h-16 lg:w-16 flex-center rounded-full bg-green-400">
-                  <HiOutlineViewGrid className="text-themeGray-dark h-8 w-8 lg:h-10 lg:w-10" />
+                <div className="w-12 h-12 bg-green-400 rounded-full lg:h-16 lg:w-16 flex-center">
+                  <HiOutlineViewGrid className="w-8 h-8 text-themeGray-dark lg:h-10 lg:w-10" />
                 </div>
               </div>
               <div className="w-3/4 lg:w-5/6">
-                <h2 className="text-sm lg:text-xl font-mono font-bold">
+                <h2 className="font-mono text-sm font-bold lg:text-xl">
                   UI/UX Enthusiast
                 </h2>
                 <p className="mt-2 text-xs lg:text-base">
@@ -38,14 +43,14 @@ const AboutMe = () => {
                 </p>
               </div>
             </div>
-            <div className="w-full py-4 pr-2 lg:px-2 flex gap-0 lg:gap-2 items-center bg-themeGray-light z-10 dark:bg-themeGray-darker rounded-xl h-1/3 transition hover:shadow-xl shadow-md">
+            <div className="z-10 flex items-center w-full py-4 pr-2 shadow-md lg:px-2 gap-0 lg:gap-2 bg-themeGray-light dark:bg-themeGray-darker rounded-xl h-1/3 transition hover:shadow-xl">
               <div className="px-4">
                 <div className="h-12 w-12 lg:h-16 lg:w-16 flex-center rounded-full bg-[#e8e8e8] dark:bg-themeGray-light">
-                  <MdSyncProblem className="text-themeGray-dark h-8 w-8 lg:h-10 lg:w-10" />
+                  <MdSyncProblem className="w-8 h-8 text-themeGray-dark lg:h-10 lg:w-10" />
                 </div>
               </div>
               <div className="w-3/4 lg:w-5/6">
-                <h2 className="text-sm lg:text-xl font-mono font-bold">
+                <h2 className="font-mono text-sm font-bold lg:text-xl">
                   Problem Solver
                 </h2>
                 <p className="mt-2 text-xs lg:text-base">
@@ -55,7 +60,26 @@ const AboutMe = () => {
             </div>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 bg-green-200 h-full p-16">Blun</div>
+        <div className="bg-themeGray-light dark:bg-themeGray-dark md:w-[480px] rounded-xl h-full ">
+          <div className="h-full pt-8 pl-8 pr-4 pb-8">
+            <p className="text-yellow-400 text-sm underline">
+              More About Me...
+            </p>
+            <h2 className="text-2xl lg:text-3xl font-mono mt-8">
+              Hello, I'm Rodrigo Nascimento
+            </h2>
+            <p className="mt-8 pr-2">
+              I am a Brazilian born web developer with a Ph.D in
+              theoretical/computational physics and an strong interest for
+              design.
+            </p>
+            <p className="mt-4 pr-2">
+              Currently, I am finishing a Fullstack Web Developer course (YH) at
+              Nackademin.
+            </p>
+            <AppButton text="Download My CV" Icon={MdPictureAsPdf} />
+          </div>
+        </div>
       </div>
     </div>
   );
