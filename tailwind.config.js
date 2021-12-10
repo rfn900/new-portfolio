ec1599fde0cd206ca4ab13b740e579805188203c
rfn900 <rfn900@gmail.com>
2021-12-10 21:43:46 +0100

    Upgrade Tailwindcss to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,45 @@
-const colors = require("tailwindcss/colors");
-const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ["Open sans", "sans-serif", ...defaultTheme.fontFamily.sans],
-      mono: ["interstate-mono", "monospace", ...defaultTheme.fontFamily.mono],
+      sans: ['Open sans', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      mono: ['interstate-mono', 'monospace', ...defaultTheme.fontFamily.mono],
     },
     extend: {
       screens: {
-        xs: "330px",
+        xs: '330px',
       },
       inset: {
-        18: "4.5rem",
+        18: '4.5rem',
       },
       colors: {
-        warmGray: colors.warmGray,
+        warmGray: colors.stone,
         themeGray: {
-          light: "#fdfbf6",
-          mediumLight: "#CCCCCB",
-          medium: "#303D3F",
-          base: "#2D2E32",
-          dark: "#282A2D",
-          darker: "#24262A",
+          light: '#fdfbf6',
+          mediumLight: '#CCCCCB',
+          medium: '#303D3F',
+          base: '#2D2E32',
+          dark: '#282A2D',
+          darker: '#24262A',
         },
         altYellow: {
-          400: "#F0C149",
+          400: '#F0C149',
         },
         altGreen: {
-          400: "#276F6B",
+          400: '#276F6B',
         },
         altOrange: {
-          400: "#F2643F",
+          400: '#F2643F',
         },
       },
     },
   },
-  variants: {
-    extend: {
-      scale: ["active", "group-hover"],
-    },
-  },
-  plugins: [require("@tailwindcss/forms")],
-};
+  plugins: [],
+}
