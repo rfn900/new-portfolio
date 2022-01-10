e008aef3ec7ef58c38e7bc9c7adc1f586fc42dc6
rfn900 <rfn900@gmail.com>
2022-01-10 13:11:38 -0300

    Add og meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,29 @@
-import Head from "next/head";
-import Footer from "../components/Footer";
-import Nav from "../components/Nav";
-import { DarkModeContext } from "../context/darkMode";
-import { useContext } from "react";
+import Head from 'next/head'
+import Footer from '../components/Footer'
+import Nav from '../components/Nav'
+import { DarkModeContext } from '../context/darkMode'
+import { useContext } from 'react'
 
 const Layout = ({ children }) => {
-  const { darkMode, setDarkMode } = useContext(DarkModeContext);
+  const { darkMode, setDarkMode } = useContext(DarkModeContext)
   return (
     <div
       className={`overflow-hidden min-h-screen ${
-        darkMode ? "dark" : ""
+        darkMode ? 'dark' : ''
       } flex flex-col font-sans `}
     >
       <Head>
         <title>Rodrigo Nascimento's Portfolio</title>
         <meta
+          name="title"
+          property="og:title"
+          content="Rodrigo Nascimento's Portfolio"
+        />
+        <meta name="type" property="og:type" content="website" />
+        <meta name="url" property="og:url" content="https://rods.dev" />
+        <meta
           name="description"
+          property="og:description"
           content="Personal web developer portfolio from Rodrigo Nascimento"
         />
         <link rel="icon" href="/rod-favicon.png" />
@@ -28,7 +36,7 @@ const Layout = ({ children }) => {
       {children}
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
