596dfbafc428259859596fd2b12e5b924c55bbf1
rfn900 <rfn900@gmail.com>
2022-02-10 19:09:07 +0100

    Fix bug with contact form submission

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,53 +1,53 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer'
 
 export default async (req, res) => {
-  const { name, email, subject, message } = req.body;
+  const { name, email, subject, message } = req.body
   if (!name || !email || !message) {
     res.status(400).json({
-      status: "error",
-      message: "Name, Email and Message fields are required",
-    });
-    return;
+      status: 'error',
+      message: 'Name, Email and Message fields are required',
+    })
+    return
   }
 
+  // TODO: write test for mailIt
   const mailIt = async () => {
     let transporter = nodemailer.createTransport({
-      host: "marsianog.com",
+      host: 'gnldm1070.siteground.biz',
       port: 465,
       secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
       },
-    });
+    })
+    console.log(process.env.EMAIL_USER, 'aqui')
     // send mail with defined transport object
     await transporter.sendMail({
       from: `"MARSian Bot" <${process.env.EMAIL_USER}>`, // sender address
-      to: "rfn900@gmail.com", // list of receivers
+      to: 'rfn900@gmail.com', // list of receivers
       subject: `From rods.dev: ${
-        typeof subject === "undefined" ? "no subject" : subject
+        typeof subject === 'undefined' ? 'no subject' : subject
       }`, // Subject line
       text: `${message}\n\nFrom: ${name}\nEmail: ${email}`, // plain text body
       html: `<b>Message Body: </b><br>"<em>${message}</em>"<br><br><b>From: </b>${name}<br><b>Email: </b> ${email}`, // plain text body
-    });
+    })
 
     return res.status(200).json({
-      status: "success",
-      message: "Your message was delivered. Thank you for your contact",
-    });
-  };
-  if (req.method === "POST") {
+      status: 'success',
+      message: 'Your message was delivered. Thank you for your contact',
+    })
+  }
+  if (req.method === 'POST') {
     try {
-      await mailIt();
+      await mailIt()
     } catch (e) {
-      res
-        .status(500)
-        .json({ status: "error", message: "Something went wrong" });
+      res.status(500).json({ status: 'error', message: 'Something went wrong' })
     }
   } else {
     res.status(405).json({
-      status: "error",
+      status: 'error',
       message: `Method ${req.method} not supported for this endpoint`,
-    });
+    })
   }
-};
+}
