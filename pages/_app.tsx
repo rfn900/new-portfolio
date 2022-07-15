a6f36e78868b6fa227f728788066a2ad0b44de77
rfn900 <rfn900@gmail.com>
2022-07-15 10:25:33 +0200

    Add missing types and fix files in /pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
+import { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { DarkModeContext } from "../context/darkMode";
 import { ActiveSectionContext } from "../context/activeSection";
 import { useState } from "react";
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   const [darkMode, setDarkMode] = useState(true);
   const [activeSection, setActiveSection] = useState(true);
   return (
